69576f540e0c0b0b25d87b0e5d373cf70abe1f57
julio <jcsmerubia@gmail.com>
2024-03-27 17:32:26 +0100

    sidebar function and styles

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -1,6 +1,12 @@
 let showMenu = document.getElementById('sidebar-icon');
 let menuMobile = document.getElementById('menu-mobile');
+let closeMenu = document.getElementById('close-menu');
 
 showMenu.addEventListener('click', ()=>{
-    menuMobile.classList.toggle('show');
+    menuMobile.style.display= 'block';
+    
+});
+
+closeMenu.addEventListener('click', ()=>{
+    menuMobile.style.display = 'none';
 });

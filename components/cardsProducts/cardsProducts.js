77c885f0e5000e85f361e85b4a2ad1e9c3217122
xavs F5 <xavsf5@MrXavs-2.local>
2024-03-27 16:05:59 +0100

    feature creation card S & card L.v1

---
+++ components/cardsProducts/cardsProducts.js
@@ -0,0 +1,24 @@
+/*const ducksProduct = [
+    {
+        id: 1.London,
+        imgPrincipal: "./img/imgDucksProducts/front_london.jpg",
+        imgV1: "./img/imgDucksProducts/v1_london.jpg",
+        imgV2: "./img/imgDucksProducts/v2_london.jpg",
+        imgV3:  ,
+        nameProduct: "London",
+        price:  "8 €",
+        text: "Desde Londres este pato de goma viene cumpliendo todas las normativas de seguridad Europeas y puedes lamerlo sin riesgo y bañarte con él."
+
+    }
+]
+
+let cardS = document.getElementsId("cardS");
+
+ducksProduct.map((x)) => {
+    cardS.innerhtml +=`
+
+    
+    
+    `
+    
+}

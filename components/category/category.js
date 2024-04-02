7fbc1a741398950b89bacc5abe2f3659c9c86083
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-04-02 15:59:07 +0200

    made dynamic components on the landing page

---
+++ components/category/category.js
@@ -0,0 +1,42 @@
+// Datos de las categorías
+const categoryData = [
+  {
+    name: "CULTURA POP",
+    imageUrl: "./assets/img/ducks/pink-panther.jpg",
+  },
+  {
+    name: "ARTISTAS",
+    imageUrl: "./assets/img/ducks/singer-duck.png",
+  },
+  {
+    name: "PELÍCULAS",
+    imageUrl: "./assets/img/ducks/chewaka-duck.png",
+  },
+  {
+    name: "PROGRAMACIÓN",
+    imageUrl: "./assets/img/ducks/css-duck.png",
+  },
+];
+
+// const divCategoryContainer = document.getElementsByClassName("category-container");
+
+// Crear cada elemento de categoría
+categoryData.forEach((category) => {
+  const categoryGridItem = document.createElement("div");
+  categoryGridItem.classList.add("category-grid-item");
+
+  const categoryName = document.createElement("p");
+  categoryName.classList.add("category-grid-name");
+  categoryName.textContent = category.name;
+  categoryGridItem.appendChild(categoryName);
+
+  const categoryImage = document.createElement("img");
+  categoryImage.src = category.imageUrl;
+  categoryGridItem.appendChild(categoryImage);
+
+  categoryGridContainer.appendChild(categoryGridItem);
+});
+
+// Agregar la sección al documento
+const container = document.querySelector(".category-container");
+container.appendChild(categorySection);

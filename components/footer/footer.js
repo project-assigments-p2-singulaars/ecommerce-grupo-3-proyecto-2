85a16c98573d06f9e437b1efa17ed988884da011
Alexandr <alexandrchichiloza@gmail.com>
2024-04-02 14:31:18 +0200

    add boxes

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -14,7 +14,7 @@ footer.appendChild(divDuckFooter);
 
 
 const duckImg = document.createElement('img');
-duckImg.setAttribute('src', './img/patito.png');
+duckImg.setAttribute('src', '../../assets/img/patito.png');
 duckImg.setAttribute('id', 'duck-img');
 footer.appendChild(duckImg);
 
@@ -32,15 +32,15 @@ footerDiv.appendChild(divSocialMedia);
 const mediaIcons =[
     {
         id: 'facebook',
-        src: './img/facebook.png'
+        src: '../../assets/img/facebook.png'
     },
     {
         id: 'twitter',
-        src: './img/twitter.png'
+        src: '../../assets/img/twitter.png'
     },
     {
         id: 'instagram',
-        src: './img/insta.png'
+        src: '../../assets/img/insta.png'
     },
 ];
 mediaIcons.forEach(obj =>{

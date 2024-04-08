66ea6d98d7a79ad787c24468afeeea5353887762
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-04-08 15:53:56 +0200

    kinda finished post functionality

--- components/CRUD/crud.js
+++ components/CRUD/crud.js
@@ -1,22 +1,25 @@
-document.getElementById('formulario').addEventListener('submit', function(event) {
+document
+  .getElementById("add-product-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
     const formData = {
-        title : document.getElementById('title').value,
-        desc
+      title: document.getElementById("title").value,
+      category: document.getElementById("category").value,
+      price: document.getElementById("price").value,
+      description: document.getElementById("description").value,
+      stock: document.getElementById("stock").value,
+    };
+    console.log(formData);
 
-
-
-    }
-    
-
-    axios.post('http://localhost:3000/productos',)
-        .then(function (response) {
-            console.log(response.data);
-            alert('Producto creado exitosamente');
-        })
-        .catch(function (error) {
-            console.error(error);
-            alert('Error al crear producto');
-        });
-});
+    axios
+      .post("http://localhost:3000/products", formData)
+      .then(function (response) {
+        console.log(response.data);
+        alert("Producto creado exitosamente");
+      })
+      .catch(function (error) {
+        console.error(error);
+        alert("Error al crear producto");
+      });
+  });

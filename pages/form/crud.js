d45a715d6f54f5a13bb6bdf307abc448fa94123d
xavimrg <javier.martin.mr@gmail.com>
2024-04-10 19:25:00 +0200

    intento estilos crud

---
+++ pages/form/crud.js
@@ -0,0 +1,114 @@
+let idForm;
+
+if (document.title === "Form") {
+    console.log("entro");
+    
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = {
+        title: document.getElementById('title').value,
+        price: document.getElementById('price').value,
+        description: document.getElementById('description').value,
+        image1: document.getElementById('image1').value,
+        imagev1: "",
+        imagev2: "",
+        imagev3: "",
+        category: document.getElementById('category').value,
+        stock: document.getElementById('stock').value,
+    };
+
+    axios.post('http://localhost:3000/products', formData)
+        .then(function(response) {
+            console.log(response.data);
+            alert('Producto creado exitosamente');
+            // Puedes hacer otras acciones aquí, como redireccionar a otra página o recargar la lista de productos
+        })
+        .catch(function(error) {
+            console.error(error);
+            alert('Error al crear producto');
+        });
+});
+}else{
+    
+
+document.getElementById('formularioUpdate').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = {
+        id: idForm,
+        title: document.getElementById('title').value,
+        price: document.getElementById('price').value,
+        description: document.getElementById('description').value,
+        image1: document.getElementById('image1').value,
+        imagev1: "",
+        imagev2: "",
+        imagev3: "",
+        category: document.getElementById('category').value,
+        stock: document.getElementById('stock').value,
+    };
+
+    axios.put(`http://localhost:3000/products/${idForm}`, formData)
+        .then(function(response) {
+            console.log(response.data);
+            alert('Producto creado exitosamente');
+            // Puedes hacer otras acciones aquí, como redireccionar a otra página o recargar la lista de productos
+        })
+        .catch(function(error) {
+            console.error(error);
+            alert('Error al crear producto');
+        });
+});
+window.onload = cargarProductos;
+}
+
+
+function cargarProductos() {
+    axios.get('http://localhost:3000/products')
+        .then(function(response) {
+            const productos = response.data;
+
+            const select = document.getElementById('productos');
+            select.innerHTML = '';
+
+            productos.forEach(function(producto) {
+                const option = document.createElement('option');
+                option.value = producto.id;
+                option.textContent = producto.title;
+                select.appendChild(option);
+            });
+
+            select.addEventListener('change', function() {
+                const selectedProductId = this.value;
+                const selectedProduct = productos.find(producto => producto.id === selectedProductId);
+                if (selectedProduct) {
+                    idForm = selectedProduct.id
+                    document.getElementById('title').value = selectedProduct.title;
+                    document.getElementById('category').value = selectedProduct.category;
+                    document.getElementById('price').value = selectedProduct.price;
+                    document.getElementById('description').value = selectedProduct.description;
+                    document.getElementById('stock').value = selectedProduct.stock;
+                }
+            });
+        })
+        .catch(function(error) {
+            console.error('Error al cargar los productos:', error);
+        });
+}
+
+
+document.getElementById('borrarProducto').addEventListener('click', function() {
+    const productId = idForm; // Obtén el ID del producto que deseas borrar
+
+    // Realiza una solicitud DELETE al servidor para borrar el producto
+    axios.delete(`http://localhost:3000/products/${idForm}`)
+        .then(function(response) {
+            console.log(response.data);
+            alert('Producto eliminado exitosamente');
+            // Puedes hacer otras acciones aquí, como redireccionar a otra página o recargar la lista de productos
+        })
+        .catch(function(error) {
+            console.error(error);
+            alert('Error al eliminar producto');
+        });
+});
